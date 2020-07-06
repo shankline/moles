4b690546dc845167e95bdfd31a94fbc9b71b9ddf
STELLAMARUH <stellamaruh@gmail.com>
2020-07-06 18:37:52 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+c@@ -0,0 +1,9 @@
+const links = document.querySelector(".links");
+const burger = document.querySelector(".burger-all");
+
+burger.addEventListener("click", ()=>{
+
+    links.classList.toggle("show-links");
+
+
+}); 
+ 
